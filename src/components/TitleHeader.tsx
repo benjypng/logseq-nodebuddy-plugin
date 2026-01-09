2e7cb5722de43235290c90cdc3403fb7675de572
benjypng <47936793+benjypng@users.noreply.github.com>
2026-01-09 14:46:03 +0800

    chore: add page title to chat box

--- src/components/TitleHeader.tsx
+++ src/components/TitleHeader.tsx
@@ -1,14 +1,28 @@
-import { Code, Group, Text } from '@mantine/core'
+import { Badge, Code, Group, rem, Stack, Text } from '@mantine/core'
+import { IconFileText } from '@tabler/icons-react'
 
+import { useLogseqPage } from '../hooks'
 import { getModelNameFromSettings } from '../utils'
 
 export const TitleHeader = () => {
+  const { page } = useLogseqPage()
+
   return (
-    <Group align="flex-end" justify="space-between" p="sm">
-      <Text fw={700} size="lg">
-        NodeBuddy
-      </Text>
-      <Code>{getModelNameFromSettings()}</Code>
+    <Group align="center" justify="space-between" p="sm">
+      <Stack gap={0}>
+        <Text fw={700} size="lg">
+          NodeBuddy
+        </Text>
+        <Code>{getModelNameFromSettings()}</Code>
+      </Stack>
+      <Badge
+        variant="filled"
+        leftSection={
+          <IconFileText style={{ width: rem(12), height: rem(12) }} />
+        }
+      >
+        {page?.title}
+      </Badge>
     </Group>
   )
 }
